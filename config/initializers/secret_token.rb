6b2faa339f95c23c513b070ebf17fccdbd89827e
Lujaw <lujaw@ubuntu.ubuntu-domain>
2011-12-21 22:44:18 +0545

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudTranslation::Application.config.secret_token = 'aa1da5c3b668eb6f0930cfbf1bbc81ee500229164b3cae6b88d5bd1b40f43b3767a0ea16f511e84e7e7f6a2aa175a1a0e1254b50be300e5f9c508b3a356911b0'
