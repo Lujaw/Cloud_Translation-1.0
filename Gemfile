c1b30eba73576dde78dc3be9dc9bb584aa0ad8ff
Lujaw Shrestha <lujaw@hotmail.com>
2011-12-22 12:16:22 +0545

    Work chunking mechanism inplemented

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'devise'
 gem 'capybara'
 gem 'autotest'
 gem 'formtastic'
+gem 'tactful_tokenizer'
 
 
 # Gems used only for assets and not required
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,36 @@
 class Work < ActiveRecord::Base
+  require "tactful_tokenizer"
   belongs_to :client
   has_many :tasks
+  after_create :create_tasks
+
+    private
+
+
+
+
+    def sentences(num_sentences, text)
+        @tactful_tokenizer ||= TactfulTokenizer::Model.new
+        result =  @tactful_tokenizer.tokenize_text text
+        output = ""
+        0.upto([num_sentences, result.length].min - 1) do |i|
+          output << "#{result[i]} "
+        end
+        output
+      end
+
+
+      def create_tasks
+           @tactful_tokenizer ||= TactfulTokenizer::Model.new
+            result =  @tactful_tokenizer.tokenize_text(self.content)
+            0.upto(result.length - 1) do |i|
+              task1  = self.tasks.build(:content => result[i])
+              task1.save
+            end
+       end
+
 end
+
+
+
+
