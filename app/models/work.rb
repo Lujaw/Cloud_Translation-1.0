a4b5b13e906ea4eaff40900f3599672f4f0d648f
Lujaw Shrestha <lujaw@hotmail.com>
2011-12-28 10:11:07 +0545

    Credits added to client and workers

--- app/models/work.rb
+++ app/models/work.rb
@@ -8,9 +8,9 @@ class Work < ActiveRecord::Base
       def create_tasks
            @tactful_tokenizer ||= TactfulTokenizer::Model.new
             result =  @tactful_tokenizer.tokenize_text(self.content)
-            task_reward = self.reward/result.length
+            task_reward = (self.reward / result.length)
             0.upto(result.length - 1) do |i|
-              task1  = self.tasks.build(:content => result[i], :reward =>task_reward)
+              task1  = self.tasks.build(:content => result[i], :reward => task_reward)
               task1.save
             end
        end
